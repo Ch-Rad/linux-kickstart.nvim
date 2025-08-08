6ccb640d4a383eea17a0b98c46afa2fa1078d434
Ch-Rad <radixx030107@gmail.com>
2025-08-08 22:22:29 +0530

    Added buffer creating and resizing keymaps

--- lua/custom/keymaps/init.lua
+++ lua/custom/keymaps/init.lua
@@ -12,8 +12,13 @@ return {
   -- Powershell Terminal
   vim.keymap.set('n', '<leader>t', '<cmd>split (:sp)<CR><cmd>term<CR>', { desc = 'Open Terminal in current dir' }),
 
-  -- Busted Lua Shortcut
-  -- vim.keymap.set('n', '<leader>lb', function()
-  --   vim.cmd('TermExec cmd="C:\\msys64\\mingw64\\bin\\busted.bat .\\' .. vim.fs.basename(vim.fn.expand '%:r') .. '_spec.lua"')
-  -- end, { desc = 'Run tests for a lua project using buster (<file>_spec.lua should be in the same dir)' }),
+  -- Resizeing Buffers
+  vim.keymap.set('n', '<leader>p', '<cmd>resize +5<CR>', { desc = 'Increase current window height by 10' }),
+  vim.keymap.set('n', '<leader>m', '<cmd>resize -5<CR>', { desc = 'Decrease current window height by 10' }),
+  vim.keymap.set('n', '<leader>vp', '<cmd>vertical resize +5<CR>', { desc = 'Increase current window height by 10' }),
+  vim.keymap.set('n', '<leader>vm', '<cmd>vertical resize -5<CR>', { desc = 'Decrease current window height by 10' }),
+
+  -- Buffer splitting
+  vim.keymap.set('n', '<leader>sl', '<cmd>split<CR>', { desc = 'Create Horizontal Split' }),
+  vim.keymap.set('n', '<leader>sv', '<cmd>vsplit<CR>', { desc = 'Create Vertical Split' }),
 }
